a28aac75d2d8380d5c85c5d6082c40e2d5c416fd
jasonwcfan <jason.wc.fan@gmail.com>
2024-09-19 12:03:50 -0400

    cleanup

--- google_maps_scraper/main.py
+++ google_maps_scraper/main.py
@@ -13,48 +13,18 @@ finic = Finic(
     api_key=FINIC_API_KEY,
 )
 
-def get_headers_from_curl_file(curl_file: str) -> Dict[str, str]:
-    with open(curl_file, 'r') as file:
-        curl_command = file.read()
-    headers = {}
-    for line in curl_command.split('\n'):
-        if re.match(r'^\s*-H', line):
-            header_parts = line.split(':', 1)
-            if len(header_parts) == 2:
-                key = header_parts[0].replace('-H', '').strip().strip("'")
-                value=header_parts[1].strip()
-                headers[key] = value
-    return headers
-
-def parse_as_cookies(cookie_string: str) -> List[Dict[str, str]]:
-    cookies = []
-    for item in cookie_string.split('; '):
-        if '=' in item:
-            key, value = item.split('=', 1)
-            cookies.append({
-                "name": key,
-                "value": value,
-                "domain": ".amazon.com",  # Add the domain
-                "path": "/"  # Add the path
-            })
-    return cookies
-
-
 @finic.workflow_entrypoint(input_model=InputSchema)
 def main(input: InputSchema):
     query = input.query
     curl_file = input.curl_file
     output_file = input.output_file
-
-    headers = get_headers_from_curl_file(curl_file)
-    cookies = parse_as_cookies(headers.get("cookie"))
     
     places = []
 
     print("Running the Playwright script")
 
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         # context = browser.new_context()
         page = browser.new_page()
 
@@ -77,13 +47,12 @@ def main(input: InputSchema):
         results_section = page.query_selector("div[aria-label^='Results']")
         results = page.query_selector_all("a[href*='google.com/maps/place']")
         processed_results = []
-        import pdb; pdb.set_trace()
         
         while len(results) > 0:
             result = results.pop()
             if result.get_attribute("href") in processed_results:
                 continue
-            # save the url to the results
+
             places.append(result.get_attribute("href"))
             processed_results.append(page.url)
 
@@ -97,5 +66,8 @@ def main(input: InputSchema):
                 results = page.query_selector_all("a[href*='google.com/maps/place']")
         
         browser.close()
+
+        with open(output_file, "w") as file:
+            json.dump(places, file)
 
     return {"places": places}
